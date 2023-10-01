27eaabe92a4cd52f7019e1746ee90a1aadbe7b10
Anjali <anjalimagar666@gmail.com>
2023-10-01 02:22:24 -0400

    Imports the Regform component and renders it

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Regform from './app/components/Regform'
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        {/* Include the Regform component */}
+        <Regform/>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
